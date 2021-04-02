96e72dc8c33de1b73fca52dca388ae7b928667e0
tkaelbel <tkaelbel@web.de>
2021-04-02 21:16:49 +0200

    Removed eslint support

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -63,13 +63,6 @@ module.exports = function (/* ctx */) {
       // extractCSS: false,
 
       // https://quasar.dev/quasar-cli/handling-webpack
-      extendWebpack (cfg) {
-        cfg.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader'
-        })
-      },
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
